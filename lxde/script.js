a48ece92db7e1089c70c52d4e562223bf5655f70
wusuopu <admin@longchangjin.cn>
2013-12-14 22:47:33 +0000

    增加切换工作区的功能

--- lxde/script.js
+++ lxde/script.js
@@ -7,16 +7,68 @@ function Clock(obj){
 }
 Clock.prototype.setNow = function(){
   var now = new Date();
-  this.obj.innerHTML = now.getUTCHours()+":"+now.getUTCMinutes();
+  var hour = now.getUTCHours();
+  var minute = now.getUTCMinutes();
+  if (hour.toString().length < 2) {
+  	hour = "0"+hour;
+  }
+  if (minute.toString().length < 2) {
+  	minute = "0"+minute;
+  }
+  this.obj.innerHTML = hour+":"+minute;
   this.obj.title = now.getFullYear()+"年"+(now.getUTCMonth()+1)+"月"+now.getUTCDate()+"日"
 };
+
+function WorkSpace(w1, w2, t1, t2, text){
+  this.current_workspace = document.getElementById(w1);
+  this.workspace1 = document.getElementById(w1);
+  this.workspace2 = document.getElementById(w2);
+  this.tooltips1 = document.getElementById(t1);
+  this.tooltips2 = document.getElementById(t2);
+  this.text = document.getElementById(text);
+}
+WorkSpace.prototype.change = function(w) {
+  if (this.current_workspace.id == w) {
+    return ;
+  }
+  if (!!this.interval) {
+    clearInterval(this.interval);
+  }
+  this.interval = setInterval(this.hide_tooltip, 1000);
+  document.getElementById("workspace_tooltips").style.visibility = "visible";
+  this.current_workspace = document.getElementById(w);
+  this.current_workspace.style.backgroundColor = "#4E76A8";
+  this.text.innerHTML = this.current_workspace.title;
+  if (this.workspace1.id != w) {
+    this.workspace1.style.backgroundColor = "#A9A5A2";
+    this.tooltips1.style.backgroundColor = "#FFFFFF";
+    this.tooltips2.style.backgroundColor = "#4A4A4A";
+  } else {
+    this.workspace2.style.backgroundColor = "#A9A5A2";
+    this.tooltips2.style.backgroundColor = "#FFFFFF";
+    this.tooltips1.style.backgroundColor = "#4A4A4A";
+  }
+}
+WorkSpace.prototype.hide_tooltip = function(){
+  clearInterval(this.interval);
+  document.getElementById("workspace_tooltips").style.visibility = "hidden";
+}
+
+// 设置时钟
 function setClock(){
   var clock = new Clock($("#clock")[0]);
   clock.setNow();
   setInterval(function(){
   	clock.setNow();
-  }, 30000);
+
+  }, 60000);
 };
 setClock();
+// 切换工作区
+var workspace = new WorkSpace("workspace1", "workspace2",
+                                  "wk_tooltips1", "wk_tooltips2", "wk_tooltips_text");
+$('.workspace').click(function(ev){
+  workspace.change(ev.target.id);
+});
 
 });
